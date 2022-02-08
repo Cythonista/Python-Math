d328cf085c511fa8902d862a460b6fc4ba49f194
Kai Fujita <gnfx0514@gmail.com>
2022-02-08 13:02:47 +0900

    :+1:名前変更

--- PythonMath.py
+++ PythonMath.py
@@ -1,19 +1,19 @@
 class PythonMath(object):
-    def transRadix2to10(self, x):
+    def tr2to10(self, x):
         # 基数変換 2進数 → 10進数
         y = 0
         for i,j in enumerate(reversed(x)):
             y += 2**i * j
         return y
 
-    def transRadix2Dto10(self, x):
+    def tr2Dto10(self, x):
         # 基数変換 2進少数 → 10進数
         y = 0
         for i,j in enumerate(x):
             y += j / (2**(i+1))
         return y
 
-    def transRadix8to10(self, x):
+    def tr8to10(self, x):
         # 基数変換 8進数 → 10進数
         if(x >= 10):
             xx = int(x / 10) * 10
--- main.py
+++ main.py
@@ -7,21 +7,23 @@ def main():
 
     # 2進数を10進数へ変換
     x = [1, 1, 0]
-    y = math.transRadix2to10(x)
+    y = math.tr2to10(x)
     print(x)
     print(y)
 
     print('')
 
-    # 8進数を10進数へ変換
-    x = 21
-    y = math.transRadix8to10(x)
-    print(y)
-
     # 2進少数を10進数へ変換
     x = [1, 0, 1, 0, 1]
-    y = math.transRadix2Dto10(x)
+    y = math.tr2Dto10(x)
     print(y)
+
+    # 8進数を10進数へ変換
+    x = 21
+    y = math.tr8to10(x)
+    print(y)
+
+
 
 
 
